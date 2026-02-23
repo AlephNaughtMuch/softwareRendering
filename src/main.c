d098fdf97dc5583e8c1bf561903f25cb431d90e9
AlephNaughtMuch <shubhamkapoor9778@gmail.com>
2026-02-22 22:01:36 -0800

    Adding vector typedefs, making drawing rect faster, and testing projecting a cube on the render window.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+
 #include "display.h"
+#include "vector.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Declare an array of vectors/points
+//////////////////////////////////////////////////////////////////////////////////////////////
+#define N_POINTS (9 * 9 * 9)
+vec3_t cube_points[N_POINTS]; 
+vec2_t projected_points[N_POINTS];
+vec3_t camera_position = {.x = 0, .y = 0, .z = -5};
+float fov_factor = 640;
 
 // Init bool to run the render loop on
 bool is_running = false;
@@ -15,35 +26,78 @@ void setup (void) {
     color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, 
         SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
 
-}
-
-void process_input(void) {
-    SDL_Event event;
-    SDL_PollEvent(&event);
-
-    switch (event.type) {
-        case SDL_QUIT:
-            is_running = false;
-        break;
-        
-        case SDL_KEYDOWN:
-            if (event.key.keysym.sym == SDLK_ESCAPE)
-                is_running = false;
-        break;
+    
+    int point_count = 0;
+    // Start loading my array of vectors
+    // From -1 to 1 (in this 9x9x9 cube)
+    for (float x = -1; x <= 1; x += 0.25){
+        for (float y = -1; y <= 1; y += 0.25) {
+            for (float z = -1; z <= 1; z += 0.25) {
+                vec3_t new_point = { .x = x, .y = y, .z = z};
+                cube_points[point_count++] = new_point;
+            }
+        }
     }
 
 }
 
+void process_input(void) {
+    SDL_Event event;
+    
+    while(SDL_PollEvent(&event)) {
+
+        switch (event.type) {
+            case SDL_QUIT:
+                is_running = false;
+            break;
+            
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    is_running = false;
+                break;
+        }
+    }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// Function that receives a 3D vector and returns a projected 2D point.
+///////////////////////////////////////////////////////////////////////////////////////////////
+vec2_t project(vec3_t point) {
+    vec2_t projected_point = { 
+        .x = (fov_factor  * point.x) / (point.z + 0.0000001f), 
+        .y = (fov_factor * point.y) / (point.z + 0.0000001f) 
+    };
+    
+    return projected_point;
+}
+
 void update(void) {
+    for (int i = 0; i < N_POINTS; i++) {
+        vec3_t point = cube_points[i];
+
+        point.z -= camera_position.z;
+
+        // Project the point
+        vec2_t projected_point = project(point);
+
+        // Save the projected 2D vector in the array of projected points.
+        projected_points[i] = projected_point;
+
+    }
 
 }
 
 void render(void) {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
     draw_grid();
-    draw_pixel(20, 20, 0xFFFFFF00);
-    draw_rect((int) window_width/2, (int) window_height/2, 300, 150, 0xFF550055);
+
+    // Loop all projected points and render them
+    for (int i = 0; i < N_POINTS; i++) {
+        vec2_t projected_point = projected_points[i];
+        draw_rect(projected_point.x + (window_width / 2), 
+            projected_point.y + (window_height / 2), 4, 4, 0xFFFFFF00);
+    }
+    
     render_color_buffer();
     clear_color_buffer(0xFF000000);
     SDL_RenderPresent(renderer);
@@ -57,6 +111,7 @@ int main(void) {
     
     while (is_running){
         process_input();
+        if(!is_running) break;
         update();
         render();
     }
