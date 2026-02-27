8c2eb2b0b96a74b74eef3adc5e097360948973af
AlephNaughtMuch <shubhamkapoor9778@gmail.com>
2026-02-26 21:34:46 -0800

    Adding a draw triangle function

--- src/main.c
+++ src/main.c
@@ -124,13 +124,18 @@ void render(void) {
     // Loop all projected triangles and render them
     for (int i = 0; i < N_MESH_FACES; i++) {
         triangle_t triangle = triangles_to_render[i];
-        // draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFF00FFFF);
-        // draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFF00FFFF);
-        // draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFF00FFFF);
+        draw_triangle(
+            triangle.points[0].x,
+            triangle.points[0].y,
 
-        draw_line(triangle.points[0].x, triangle.points[0].y, triangle.points[1].x, triangle.points[1].y, 0xFF00FFFF);
-        draw_line(triangle.points[1].x, triangle.points[1].y, triangle.points[2].x, triangle.points[2].y, 0xFF00FFFF);
-        draw_line(triangle.points[0].x, triangle.points[0].y, triangle.points[2].x, triangle.points[2].y, 0xFF00FFFF);
+            triangle.points[1].x,
+            triangle.points[1].y,
+
+            triangle.points[2].x,
+            triangle.points[2].y,
+            
+            0xFF00FFFF
+        );
     }
     
     render_color_buffer();
