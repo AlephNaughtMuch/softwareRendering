795e256994bea9110c939de63033d5016c05328e
AlephNaughtMuch <shubhamkapoor9778@gmail.com>
2026-04-11 18:51:29 -0700

    Adding texture class, swap class, and adding basic representation of rendering faces with textures in preparation of full implementation soon.

--- src/triangle.c
+++ src/triangle.c
@@ -1,13 +1,14 @@
 #include "triangle.h"
 #include "display.h"
+#include "swap.h"
 
-void int_swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int mx, int my, uint32_t color) {
+void fill_flat_bottom_triangle (
+    int x0, int y0, 
+    int x1, int y1, 
+    int mx, int my, 
+    uint32_t color
+) 
+{
     // Find the slopes for the two triangle legs
     float inv_slope_1 = ((float) (x1 - x0)) / (y1 - y0);
     float inv_slope_2 = ((float) (mx - x0)) / (my - y0);
@@ -26,7 +27,13 @@ void fill_flat_bottom_triangle(int x0, int y0, int x1, int y1, int mx, int my, u
 
 }
 
-void fill_flat_top_triangle(int x1, int y1, int mx, int my, int x2, int y2, uint32_t color) {
+void fill_flat_top_triangle (
+    int x1, int y1, 
+    int mx, int my, 
+    int x2, int y2, 
+    uint32_t color
+) 
+{
     // Find the slopes for the two triangle legs
     float inv_slope_1 = ((float) (x2 - x1)) / (y2 - y1);
     float inv_slope_2 = ((float) (x2 - mx)) / (y2 - my);
@@ -43,7 +50,13 @@ void fill_flat_top_triangle(int x1, int y1, int mx, int my, int x2, int y2, uint
     }
 }
 
-void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+void draw_filled_triangle (
+    int x0, int y0, 
+    int x1, int y1, 
+    int x2, int y2, 
+    uint32_t color
+) 
+{
     // Sort the vertices (y0 < y1 <y2)
     // Check first and second
     if (y0 > y1) {
@@ -78,5 +91,92 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
         fill_flat_top_triangle(x1, y1, mx, my, x2, y2, color);
     }
 
+
+}
+
+void draw_textured_triangle (
+    int x0, int y0, float u0, float v0,
+    int x1, int y1, float u1, float v1,
+    int x2, int y2, float u2, float v2,
+    uint32_t* texture 
+) 
+{
+    //////////////////////////////////////////////////////////
+    // Sort the vertices (y0 < y1 <y2) ///////////////////////
+    //////////////////////////////////////////////////////////
+
+    // Check first and second
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+
+        float_swap(&u0, &u1);
+        float_swap(&v0, &v1);
+    }
+    // Check second and third
+    if (y1 > y2) {
+        int_swap(&y1, &y2);
+        int_swap(&x1, &x2);
+
+        float_swap(&u1, &u2);
+        float_swap(&v1, &v2);
+    }
+    // Check possible new first and new second
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+
+        float_swap(&u0, &u1);
+        float_swap(&v0, &v1);
+    }
+
+    //////////////////////////////////////////////////////////
+    // Render the upper part of the triangle (flat-bottom) ///
+    //////////////////////////////////////////////////////////
+
+    float inv_slope_1 = 0;
+    float inv_slope_2 = 0;
+
+    if (y1 - y0 != 0) { inv_slope_1 = (float) (x1 - x0) / abs(y1 - y0); }
+    if (y2 - y0 != 0) { inv_slope_2 = (float) (x2 - x0) / abs(y2 - y0); }
+    
+    if (y1 - y0 != 0) {
+        for (int y = y0; y <= y1; y++) {
+    
+            int x_start = x1 + (y - y1) * inv_slope_1;
+            int x_end = x0 + (y - y0) * inv_slope_2;
+    
+            if (x_end < x_start) { int_swap(&x_start, &x_end); }
+    
+            for (int x = x_start; x < x_end; x++) {
+                draw_pixel(x, y, (x % 2 == 0 && y % 2 == 0 ? 0xFFFF00FF : 0xFF000000));
+            }
+    
+        }
+    }
+
+    //////////////////////////////////////////////////////////
+    // Render the bottom part of the triangle (flat-top) /////
+    //////////////////////////////////////////////////////////
+    inv_slope_1 = 0;
+    // inv_slope_2 = 0;
+
+    if (y2 - y1 != 0) { inv_slope_1 = (float) (x2 - x1) / abs(y2 - y1); }
+    // if (y2 - y0 != 0) { inv_slope_2 = (float) (x2 - x0) / abs(y2 - y0); }
+    
+    if (y2 - y1 != 0) {
+        for (int y = y1; y <= y2; y++) {
+    
+            int x_start = x1 + (y - y1) * inv_slope_1;
+            int x_end = x0 + (y - y0) * inv_slope_2;
+    
+            if (x_end < x_start) { int_swap(&x_start, &x_end); }
+    
+            for (int x = x_start; x < x_end; x++) {
+                draw_pixel(x, y, (x % 2 == 0 && y % 2 == 0 ? 0xFFFF00FF : 0xFF000000));
+            }
+    
+        }
+    }
 
 }
